7e3a5d54296a633fb1822eca3e869c9d1e7763a3
Henry Riddall <Henry.Riddall@softwire.com>
2025-01-28 11:27:51 +0000

    Add logging

--- app.py
+++ app.py
@@ -3,10 +3,22 @@ from datetime import datetime, timezone
 
 from werkzeug.utils import redirect
 from flask_config import Config
-from data.database import initialise_database, add_order, clear_orders, count_orders, get_orders_to_display, get_queued_count, get_recently_placed_count, get_recently_processed_count
+from data.database import (
+    initialise_database,
+    add_order,
+    clear_orders,
+    count_orders,
+    get_orders_to_display,
+    get_queued_count,
+    get_recently_placed_count,
+    get_recently_processed_count,
+)
 from scheduled_jobs import initialise_scheduled_jobs
 from products import create_product_download
 import requests
+import logging
+
+logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
 app.config.from_object(Config)
 
@@ -21,21 +33,26 @@ def index():
     recently_placed_count = get_recently_placed_count()
     recently_processed_count = get_recently_processed_count()
     scenarios = [
-        { 'display': 'Add broken order', 'value': 'BrokenOrder' },
-        { 'display': 'Monitoring Load', 'value': 'HighLoad' },
-        { 'display': 'Queue Reliability', 'value': 'UnreliableProcessing' },
-        { 'display': 'System Monitoring', 'value': 'VeryHighLoad' },
-        { 'display': 'Reset to initial', 'value': 'Reset' }
+        {"display": "Add broken order", "value": "BrokenOrder"},
+        {"display": "Monitoring Load", "value": "HighLoad"},
+        {"display": "Queue Reliability", "value": "UnreliableProcessing"},
+        {"display": "System Monitoring", "value": "VeryHighLoad"},
+        {"display": "Reset to initial", "value": "Reset"},
     ]
 
     return render_template(
-        "layout.html", orders=orders, queue_count=queue_count, recently_placed_count=recently_placed_count,
-        recently_processed_count=recently_processed_count, scenarios=scenarios
+        "layout.html",
+        orders=orders,
+        queue_count=queue_count,
+        recently_placed_count=recently_placed_count,
+        recently_processed_count=recently_processed_count,
+        scenarios=scenarios,
     )
+
 
 @app.route("/count")
 def count():
-    return { 'count': count_orders() }
+    return {"count": count_orders()}
 
 
 @app.route("/new", methods=["POST"])
@@ -56,22 +73,24 @@ def new_order():
 def set_scenario():
     scenario = request.form["scenario"]
 
-    if scenario == 'BrokenOrder':
-        product = 'Product from the future'
+    if scenario == "BrokenOrder":
+        product = "Product from the future"
         download = create_product_download(product)
-        add_order('Product from the future', 'Me', '3000-01-01T12:00:00Z', None, download)
-        return redirect('/')
+        add_order(
+            "Product from the future", "Me", "3000-01-01T12:00:00Z", None, download
+        )
+        return redirect("/")
 
-    if scenario == 'Reset':
+    if scenario == "Reset":
         clear_orders()
 
     response = requests.post(
-        app.config["FINANCE_PACKAGE_URL"] + "/scenario",
-        json=scenario
+        app.config["FINANCE_PACKAGE_URL"] + "/scenario", json=scenario
     )
     response.raise_for_status()
 
-    return redirect('/')
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run()
--- scheduled_jobs.py
+++ scheduled_jobs.py
@@ -30,17 +30,19 @@ def process_orders(app):
         }
 
         response = requests.post(
-            app.config["FINANCE_PACKAGE_URL"] + "/ProcessPayment",
-            json=payload
+            app.config["FINANCE_PACKAGE_URL"] + "/ProcessPayment", json=payload
         )
+
+        app.logger.info("Response from endpoint: " + response.text)
 
         response.raise_for_status()
 
         order.set_as_processed()
         save_order(order)
 
+
 def get_queue_of_orders_to_process():
     allOrders = get_all_orders()
     queuedOrders = filter(lambda order: order.date_processed == None, allOrders)
-    sortedQueue = sorted(queuedOrders, key= lambda order: order.date_placed)
+    sortedQueue = sorted(queuedOrders, key=lambda order: order.date_placed)
     return list(sortedQueue)
